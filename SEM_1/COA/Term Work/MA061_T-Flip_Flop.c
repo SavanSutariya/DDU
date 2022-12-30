c29f6392e456e47103c9650ff37dd9b1411ddefc
Savan Sutariya <savanpatel5248@gmail.com>
2022-12-30 10:57:53 +0530

    TermWork[COA]

--- SEM_1/COA/Term Work/MA061_T-Flip_Flop.c
+++ SEM_1/COA/Term Work/MA061_T-Flip_Flop.c
@@ -1,29 +1,37 @@
-// T Flip Flop using C
-// Created: 2016-02-01
-
-#include <stdio.h>
-#include <stdlib.h>
-int main()
+// T flip flop
+#include<stdio.h>
+int Q = 0, Q_comp = 1;
+int nand(int a,int b,int c)
 {
-    int t;
-    int q = 0;
-    int qn = 1;
-    printf("Enter the number of inputs: ");
-    scanf("%d", &t);
-    int input[t];
-    printf("Enter the inputs: ");
-    for (int i = 0; i < t; i++)
+    return !(a&&b&&c);
+}
+void T_Flip_Flop(int T,int clc)
+{
+    int R = nand(Q_comp,T,clc);
+    int S = nand(Q,T,clc);
+
+    if (!R)
     {
-        scanf("%d", &input[i]);
+        Q = nand(R,Q_comp,1);
+        Q_comp = nand(S,Q,1);
     }
-    for (int i = 0; i < t; i++)
+    else
     {
-        if (input[i] == 1)
-        {
-            qn = !qn;
-            q = !q;
-        }
-        printf("Q%d = %d Qn%d = %d -- %d \n", i, q, i, qn, input[i]);
+        Q_comp = nand(S,Q,1);
+        Q = nand(R,Q_comp,1);
+    }
+}
+int main(){
+    int T = 1,clc=1;
+    printf("Enter greater than 1 to exit:");
+    while (1){
+        printf("\nEnter T and clc: ");
+        scanf("%d",&T);
+        if (T>1) break;
+        scanf("%d",&clc);
+        printf("Previous[%d %d]",Q,Q_comp);
+        T_Flip_Flop(T,clc);
+        printf(" Next[%d %d]",Q,Q_comp);
     }
     return 0;
 }
